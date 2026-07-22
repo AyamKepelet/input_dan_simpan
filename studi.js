3525acf11dfac56342092b06495126d8049702a2
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2026-07-22 10:20:37 +0700

    done

--- studi.js
+++ studi.js
@@ -7,16 +7,16 @@ const rl = readline.createInterface({
 })
 console.log(`present students`);
 
-    let id = 0
 rl.question("your name? ", (name) =>{
     rl.question("your class? ", (classes)=>{
         rl.question("present or not ",(present) => {
-    let contact = {id: ++id,name,classes,present}
     const file = fs.readFileSync("storage.json","utf-8")
     const contacts = JSON.parse(file)
+    const nextId = contacts.length > 0 ? Math.max(...contacts.map(c => c.id)) + 1 : 1
+    let contact = {id: nextId,name,classes,present}
     contacts.push(contact)
     fs.writeFileSync("storage.json",JSON.stringify(contacts,null,2))
-    present == "Present" ? console.log("Nice Kid") : console.log("Absen");
+    present == "Present" || "present" ? console.log("good") : console.log("drop out aja");
     console.log(`Your Id: ${contact.id}`);
     tampilkanData()
         })
